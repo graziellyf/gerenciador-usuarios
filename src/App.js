e551435e7b6b7ddcd73d9796e53f4402bd1078a7
graziellyf <132098875+graziellyf@users.noreply.github.com>
2023-05-19 17:21:55 -0300

    aula conradito chokito 19-05

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import"./App.css"
+
+import Listagem from './Componentes/Listagem'
 
 function App() {
+
+//como funciona objetos em javascript
+const usuario = {
+  // chave: valor
+  nome:"Grazy",
+  idade: 18,
+  status:true,
+  endereco: {
+    rua: "Rua X",
+    numero: 139,
+    complemento: "ap 20"
+  },
+  vacinasCovid19: ["Pfizer", "Pfizer", "Pfizer", "J&J"]
+}
+
+//console.log(usuario)
+console.log(usuario.endereco.rua)
+
+
+ //como usar um array com objetos dentro 
+  const usuarios = [
+    
+     { nome: "Grazy", status: true },
+     { nome: "Luana", status: true },
+     { nome: "Egmar", status: true },
+     { nome: "Gabriel", status: false },
+    ]
+
+   console.log(usuarios)
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      
+    <div className="d-flex justify-content-around"> 
+
+        <Listagem usuarios={usuarios} />
+
+        <div className="caixa">
+          <h2> Cadastro </h2>
+        </div>
+
+    </div>
+
     </div>
   );
 }
---
+++ src/Componentes/Listagem.js
@@ -0,0 +1,21 @@
+function Listagem( props ){
+    return(
+        <div className="caixa">
+          <h2> Listagem </h2>
+
+          {
+            props.usuarios.map( item => 
+            <div style={{border: "1px solid #CCC"}} >
+                <p> Nome: {item.nome} </p> 
+                <p> Status: {item.status == true ? "Ativo" : "Banido"} </p>
+               </div>
+
+            )
+          }
+
+        </div>
+        
+    )
+}
+
+export default Listagem;
